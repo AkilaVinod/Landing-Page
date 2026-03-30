d42146f76fb4c85fb4f4c29ac88ef7d3ce301a54
AkilaVinod <akilavinod369@gmail.com>
2026-03-30 18:59:34 +0530

    added animation for menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,8 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
 export default function Navbar(){
+    const [mobileMenuIsOpen,setMobileMenuIsOpen] = useState(false);
     return(
         <nav className="fixed top-0 w-full z-50 transition-all duration-300 bg-slate-950/20 backdrop-blur-sm">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -13,7 +17,7 @@ export default function Navbar(){
                         </span>
                     </div>
                     {/* Nav Links */}
-                    <div className="flex items-center space-x-6 lg:space-x-8">
+                    <div className="hidden md:flex items-center space-x-6 lg:space-x-8">
                         <a href="#features" className="text-gray-300 hover:text-white text-sm lg:text-base">
                             Features
                         </a>
@@ -24,8 +28,25 @@ export default function Navbar(){
                             Testimonials
                         </a>
                     </div>
+                    <button onClick={() => setMobileMenuIsOpen((prev)=>!prev)} className="md:hidden p-2 text-gray-300 hover:text-white">
+                        {mobileMenuIsOpen? (<X className="w-5 h-5 sm:w-6 sm:h-6"/>):(<Menu className="w-5 h-5 sm:w-6 sm:h-6"/>)}
+                    </button>
                 </div>
             </div>
+            {mobileMenuIsOpen && 
+            <div className="md:hidden bg-slate-900/95 backdrop-blur-lg border-t border-slate-800 animate-in slide-in-from-top duration-300">
+                <div className="px-4 py-4 sm:py-6 space-y-3 sm:space-y-4">
+                    <a href="#features" className=" block text-gray-300 hover:text-white text-sm lg:text-base">
+                            Features
+                        </a>
+                        <a href="#pricing" className="block text-gray-300 hover:text-white text-sm lg:text-base">
+                            Pricing
+                        </a>
+                        <a href="#testimonials" className="block text-gray-300 hover:text-white text-sm lg:text-base">
+                            Testimonials
+                        </a>
+                </div>
+            </div>}
         </nav>
     )
 }
